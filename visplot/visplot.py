b32f894af30b48de2122a046bdcd54854f48521f
Oliver Cordes <ocordes@astro.uni-bonn.de>
2021-04-11 14:52:59 +0200

    - start with the work

---
+++ visplot/visplot.py
@@ -0,0 +1,29 @@
+"""
+visplot.py
+
+written by: Oliver Cordes 2021-04-11
+changed by: Oliver Cordes 2021-04-11
+
+"""
+
+
+import os, sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.visualization import astropy_mpl_style, quantity_support
+plt.style.use(astropy_mpl_style)
+quantity_support()
+
+
+
+
+import astropy
+
+
+
+"""
+main function 
+"""
+def main():
+    pass
